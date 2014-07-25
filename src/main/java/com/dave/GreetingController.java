54db845829a0374e35a2507beca8a3019946d175
davemilligan <davidgeraldmilligan@gmail.com>
2014-07-25 10:29:36 +0100

    New message return added to GreetingController

--- src/main/java/com/dave/GreetingController.java
+++ src/main/java/com/dave/GreetingController.java
@@ -2,12 +2,16 @@ package com.dave;
 
 /**
  * Tomcat
- * curl -X GET http://localhost:8080/1
+ * curl -X GET http://localhost:8080/greeting/1
+ * curl -X GET http://localhost:8080/greeting/list
  * curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"id":"1","content":"this is daves content"}' http://localhost:8080/greeting/
- * curl -H "Accept: application/json" -H "Content-type: application/json" -X PUT -d '{"id":"1","content":"this is daves content"}' http://localhost:8080/greeting/1
+ * curl -H "Accept: application/json" -H "Content-type: application/json" -X PUT -d '{"id":"1","content":"this is daves content"}' http://localhost:8080/greeting/
  * curl -X DELETE http://localhost:8080/greeting/1
+ * curl -X DELETE http://localhost:8080/greeting/
  */
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.*;
@@ -16,32 +20,40 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/greeting")
 public class GreetingController {
 
-    private static final String createTemplate = "Hello, %s!";
+    private static final String createTemplate = "Created, %s!";
+    private static final String readTemplate = "Hello for %s time!";
+    private static final String updateTemplate = "Updated, %s!";
     private static final String deleteTemplate = "Deleted, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    private final AtomicLong readCounter = new AtomicLong();
  
     
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    public Greeting create(@RequestBody(required=true) Greeting greeting) {
-    	greeting.setContent("Greeting received!");
-    	return greeting;
+    public Message create(@RequestBody(required=true) Greeting greeting) {
+    	return new Message(String.format(createTemplate, greeting.getId()));
     }
     
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Greeting read(@PathVariable long id) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(createTemplate, id));
+        return new Greeting(id, String.format(readTemplate, readCounter.incrementAndGet()));
     }
     
-    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public Greeting update(@PathVariable long id, @RequestBody(required=true) Greeting greeting) {
-    	greeting.setContent("Greeting updated!");
-    	return greeting;
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public List<Greeting> read() {
+        return new ArrayList<Greeting>();
+    }
+    
+    @RequestMapping(value = "/", method = RequestMethod.PUT)
+    public Message update(@RequestBody(required=true) Greeting greeting) {
+    	return new Message(String.format(updateTemplate, greeting.getId()));
     }
     
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public Greeting delete(@PathVariable long id) {
-    	return new Greeting(counter.incrementAndGet(),
-                String.format(deleteTemplate, id));
+    public Message delete(@PathVariable long id) {
+    	return new Message(String.format(deleteTemplate, id));
+    }
+    
+    @RequestMapping(value = "/", method = RequestMethod.DELETE)
+    public Message delete() {
+    	return new Message("All deleted");
     }
 }
